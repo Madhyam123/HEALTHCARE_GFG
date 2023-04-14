d6ef1b3ec5c0d2406e6c815f7e1bc405b91f71da
ADITYA-003 <adityamamgai28@gmail.com>
2023-04-14 16:22:09 +0530

    modified app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
+import jwt_decode from "jwt-decode";
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
@@ -13,8 +13,42 @@ import Stattics from './components/Stattics';
 import Company from './components/Company';
 import Chatbot from './components/Chatbot';
 function App() {
+  const [user,SetUser] = useState({});
+  function handleCallbackResponse(response){
+    console.log("Encoded JWT ID Token:"+response.credential);
+    var userObject=jwt_decode(response.credential);
+    console.log(userObject);
+    SetUser(userObject);
+    document.getElementById("signInDiv").hidden=true;
+  }
+  function handleSignOut(event){
+    SetUser({});
+    document.getElementById("signInDiv").hidden=false;
+  }
+  useEffect(()=>{
+    window.google.accounts.id.initialize({
+      client_id:"239663637489-7h5db2mcf48unqlacagv4vhupnsj2t1m.apps.googleusercontent.com",
+      callback:handleCallbackResponse
+    });
+    window.google.accounts.id.renderButton(
+      document.getElementById("signInDiv"),
+      {theme:"outline",size:"large"}
+    );
+    window.google.accounts.id.prompt();
+  },[]);
   return (
     <div className='dark:bg-black'>
+      <div id='signInDiv'></div>
+      {Object.keys(user).length!==0 &&
+        <button className='p-6 text-white bg-green-500' onClick={(e)=>handleSignOut(e)}>Signout</button>
+        
+      }
+      {user &&
+      <div>
+        <img src={user.picture} alt="" />
+        <h3 className='text-blue-700'>{user.name}</h3>
+      </div>
+      }
       <Router>
         <Navbar />
         <Switch>
@@ -43,4 +77,4 @@ function App() {
   );
 }
 
-export default App;
+export default App;
