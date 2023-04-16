030dc5043da69c9a03348a94b2b90a542be297b9
Madhyam123 <madhyambharati@gmail.com>
2023-04-16 19:54:01 +0530

    hello2

--- src/components/Bmi.jsx
+++ src/components/Bmi.jsx
@@ -1,70 +1,70 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 
 
 import '../Bmi.css';
 function Bmi(props) {
 
-        const [height, setHeightError] = useState('');
-        const [weight, setWeightError] = useState('');
-        const [bmiResult, setBmiResult] = useState('');
-   
-        let button = document.getElementById('btn');
+    const [height, setHeightError] = useState('');
+    const [weight, setWeightError] = useState('');
+    const [bmiResult, setBmiResult] = useState('');
 
-        const handleCalOverall=()=>{
-            
-            const height = parseInt(document.getElementById('height').value);
-            const weight = parseInt(document.getElementById('weight').value);
+    let button = document.getElementById('btn');
 
-        if(height === '' || isNaN(height) || (height <= 0)){
+    const handleCalOverall = () => {
+
+        const height = parseInt(document.getElementById('height').value);
+        const weight = parseInt(document.getElementById('weight').value);
+
+        if (height === '' || isNaN(height) || (height <= 0)) {
             setHeightError('Please provide a valid height');
-        }else{
+        } else {
             setHeightError('');
-    
+
         }
 
-        if(weight === '' || isNaN(weight) || (weight <= 0)){
+        if (weight === '' || isNaN(weight) || (weight <= 0)) {
             setWeightError('Please provide a valid weight');
-        }else{
+        } else {
             setWeightError('');
         }
 
-        if(height> 0 && weight >0){
-            const bmi = (weight / ((height*height)/10000)).toFixed(2);
-          
-            if(bmi < 18.6){
-                setBmiResult('Under weight : '+bmi);
-               
-            }else if(bmi >= 18.6 && bmi < 24.9){
+        if (height > 0 && weight > 0) {
+            const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+
+            if (bmi < 18.6) {
+                setBmiResult('Under weight : ' + bmi);
+
+            } else if (bmi >= 18.6 && bmi < 24.9) {
                 setBmiResult('Normal : ');
-            }else{
-                setBmiResult('Over weight : '+bmi);
+            } else {
+                setBmiResult('Over weight : ' + bmi);
             }
-        }else{
+        } else {
             alert('The form has errors');
             setBmiResult('');
 
         }
     };
-  return (props.trigger)?(
-    <div className="wrapper">
+    return (props.trigger) ? (
+        <div className="wrapper">
 
-        <p>Height (CM):</p>      <button className="close-btn" onClick = {()=>props.setTrigger(false)}>X</button>
-            <input type="text"  id="height"/><br/><span>{setHeightError}</span>
-        
-
-        <p>Weight (KG):</p>
-            <input type="text" id="weight"/><br/><span>{setWeightError}</span>
-   
-
-        <button onClick={handleCalOverall} id="btn">Calculate</button>
- 
-        <p>{bmiResult}</p>
+            <p>Height (CM):</p>      <button className="close-btn" onClick={() => props.setTrigger(false)}>X</button>
+            <input type="text" id="height" /><br /><span>{setHeightError}</span>
 
 
-    </div>
-      
-):"";
-  
+            <p>Weight (KG):</p>
+            <input type="text" id="weight" /><br /><span>{setWeightError}</span>
+
+
+            <button onClick={handleCalOverall} id="btn">Calculate</button>
+
+            <p>{bmiResult}</p>
+
+
+        </div>
+
+    ) : "";
+
 }
 
 export default Bmi;
